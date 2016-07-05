f755bfe4966b21a37e5762b4c1b580cb8ce20ba6
taperlawyer <taperlawyer@gmail.com>
2016-07-05 23:31:47 +0200

    Add Trip Details component

---
+++ app/trip-details.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'trip-details',
+    template: 'Trip details'
+})
+export class TripDetailsComponent { }
--- app/trip.component.ts
+++ app/trip.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TripDetailsComponent } from './trip-details.component';
+import { SearchResult } from './search-result';
 
 @Component({
     selector: 'trip',
+    directives: [TripDetailsComponent],
     template: `
         <label>From: <input/></label>
         <label>To: <input/></label>
         <button>Search</button>
+        <trip-details *ngIf="searchResult"></trip-details>
         `
 })
-export class TripComponent { }
+export class TripComponent {
+    searchResult: SearchResult;
+}
