bd2bc6ee94de8564f4261bcbbcf01695c56159dd
taperlawyer <taperlawyer@gmail.com>
2016-07-07 13:27:41 +0200

    Add more mock trips

--- app/mock-trips.ts
+++ app/mock-trips.ts
@@ -23,4 +23,50 @@ export var TRIPS: Trip[] = [{
             track: 'B'
         }
     }]
+}, {
+    legs: [{
+        name: 'Buss 61',
+        type: 'BUS',
+        id: '1',
+        direction: 'Vänersborg Resecentrum',
+        origin: {
+            name: 'Trollhättan, Resecentrum, Trollhättan',
+            type: 'ST',
+            id: '2',
+            routeIndex: '8',
+            date: new Date(),
+            track: 'L'
+        },
+        destination: {
+            name: 'NÄL, Trollhättan',
+            type: 'ST',
+            id: '3',
+            routeIndex: '22',
+            date: new Date(),
+            track: 'B'
+        }
+    }]
+}, {
+    legs: [{
+        name: 'Buss 61',
+        type: 'BUS',
+        id: '1',
+        direction: 'Vänersborg Resecentrum',
+        origin: {
+            name: 'Trollhättan, Resecentrum, Trollhättan',
+            type: 'ST',
+            id: '2',
+            routeIndex: '8',
+            date: new Date(),
+            track: 'L'
+        },
+        destination: {
+            name: 'NÄL, Trollhättan',
+            type: 'ST',
+            id: '3',
+            routeIndex: '22',
+            date: new Date(),
+            track: 'B'
+        }
+    }]
 }];
