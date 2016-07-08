88495bcd3585a938bd0350db0bf6cf87bc8154ee
taperlawyer <taperlawyer@gmail.com>
2016-07-08 21:54:06 +0200

    Show departure and arrival times in result

--- app/search.component.ts
+++ app/search.component.ts
@@ -11,7 +11,11 @@ import { Trip } from './trip';
         <button (click)="search()">Search</button>
         <ul class="trip-list" *ngIf="trips">
             <li *ngFor="let trip of trips">
-                {{trip.legs[0].name}}
+                <span class="column">{{trip.legs[0].origin.date | date:'shortTime'}}</span>
+                <span class="column">{{trip.legs[trip.legs.length-1].destination.date | date:'shortTime'}}</span>
+                <span class="column">
+                    <span *ngFor="let leg of trip.legs">{{leg.name}}</span>
+                </span>
             </li>
         </ul>
         `
