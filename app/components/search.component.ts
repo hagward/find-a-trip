94dd0f14bb8c1445851c6b8ff546c914b37928d8
taperlawyer <taperlawyer@gmail.com>
2016-08-06 13:19:36 +0200

    Add flex table styling for search results

--- app/components/search.component.ts
+++ app/components/search.component.ts
@@ -9,31 +9,39 @@ import { SearchService } from '../services/search.service';
         <label>From: <input/></label>
         <label>To: <input/></label>
         <button (click)="search()">Search</button>
-        <ul class="trip-list" *ngIf="trips">
-            <li *ngFor="let trip of trips">
-                <div *ngIf="trip.Leg.length">
-                    <span class="column">{{trip.Leg[0].Origin.date}}</span>
-                    <span class="column">{{trip.Leg[0].Origin.name}}</span>
+        <div class="trip-list" *ngIf="trips">
+            <div class="header-row">
+                <span class="col-2">Startdatum</span>
+                <span class="col-2">Startdest.</span>
+                <span class="col-2">Slutdatum</span>
+                <span class="col-2">Slutdest.</span>
+                <span class="col-4">Färdmedel</span>
+            </div>
 
-                    <span class="column">{{trip.Leg[trip.Leg.length-1].Destination.date}}</span>
-                    <span class="column">{{trip.Leg[trip.Leg.length-1].Destination.name}}</span>
+            <div *ngFor="let trip of trips">
+                <div class="row" *ngIf="trip.Leg.length">
+                    <span class="col-2">{{trip.Leg[0].Origin.date}}</span>
+                    <span class="col-2">{{trip.Leg[0].Origin.name}}</span>
 
-                    <span class="column">
+                    <span class="col-2">{{trip.Leg[trip.Leg.length-1].Destination.date}}</span>
+                    <span class="col-2">{{trip.Leg[trip.Leg.length-1].Destination.name}}</span>
+
+                    <span class="col-4">
                         <span *ngFor="let leg of trip.Leg">{{leg.name}}</span>
                     </span>
                 </div>
 
-                <div *ngIf="!trip.Leg.length">
-                    <span class="column">{{trip.Leg.Origin.date}}</span>
-                    <span class="column">{{trip.Leg.Origin.name}}</span>
+                <div class="row" *ngIf="!trip.Leg.length">
+                    <span class="col-2">{{trip.Leg.Origin.date}}</span>
+                    <span class="col-2">{{trip.Leg.Origin.name}}</span>
 
-                    <span class="column">{{trip.Leg.Destination.date}}</span>
-                    <span class="column">{{trip.Leg.Destination.name}}</span>
+                    <span class="col-2">{{trip.Leg.Destination.date}}</span>
+                    <span class="col-2">{{trip.Leg.Destination.name}}</span>
 
-                    <span class="column">{{trip.Leg.name}} ({{trip.Leg.direction}})</span>
+                    <span class="col-4">{{trip.Leg.name}} ({{trip.Leg.direction}})</span>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
         `
 })
 export class SearchComponent {
