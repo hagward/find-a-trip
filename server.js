5d0a5697f49da9fd22d4e0318618b9749a7a7c12
hagward <taperlawyer@gmail.com>
2016-09-03 20:20:46 +0200

    Add route for getting an auth token

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const https = require('https');
+const vasttrafik = require('./vasttrafik.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', express.static('public'));
+
+app.get('/token', (req, res) => {
+    authorize(vasttrafik.host, vasttrafik.path, vasttrafik.key, vasttrafik.secret, '1')
+        .then((token) => res.send(token));
+});
 
 app.listen(port, () => {
     console.log('Server listening to port %d!', port);
