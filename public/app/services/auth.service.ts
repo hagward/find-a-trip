ea1c05641f3b0976ebc26fbdc4584fa25827dc14
hagward <taperlawyer@gmail.com>
2016-09-12 13:46:25 +0200

    Add caching of access token

--- public/app/services/auth.service.ts
+++ public/app/services/auth.service.ts
@@ -3,13 +3,23 @@ import { Headers, Http, Response, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class AuthService {
+    private expires: number;
+    private token: string;
+
     constructor(private http: Http) {}
 
     authorize() {
         return new Promise((resolve, reject) => {
-            this.http.get('/token').subscribe(response => {
-                resolve(response.json().access_token);
-            });
+            if (Date.now() < this.expires) {
+                resolve(this.token);
+            } else {
+                this.http.get('/token').subscribe(response => {
+                    const json = response.json();
+                    this.expires = Date.now() + Number(json.expires_in) * 1000;
+                    this.token = json.access_token;
+                    resolve(this.token);
+                });
+            }
         });
     }
 }
